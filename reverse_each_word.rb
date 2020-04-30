02da85bb1939d05617df9f9de45e6f1a018ca192
Bobby Neal <rsneal01@gmail.com>
2020-04-30 16:58:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
   new_sentence = []
-  new_sentence << words.each do |word|
-    word.reverse
+  words.each do |word|
+    new_sentence << word.reverse
   end
  new_sentence 
 end
